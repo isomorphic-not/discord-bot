faa6fcf4e0d75b374218af169bbce26d75d0cfef
Andrew Keller <abkgit@gmail.com>
2024-04-20 13:27:30 -0400

    add all cmc ids

--- cutils.py
+++ cutils.py
@@ -9,10 +9,29 @@ if TYPE_CHECKING:
     from _typeshed import SupportsKeysAndGetItem
 
 
+def getAll(cmc_api: str) -> None:
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map"
+    headers: SupportsKeysAndGetItem[str, str] = {
+        "Accepts": "application/json",
+        "X-CMC_PRO_API_KEY": cmc_api,
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    response = session.get(url)
+    json_data = json.loads(response.text)
+    data_len = len(json_data["data"])
+    results = {}
+    for i in range(data_len):
+        results[f"!{json_data['data'][i]['symbol'].lower()}"] = {
+            "name": json_data["data"][i]["slug"],
+            "id": str(json_data["data"][i]["id"]),
+        }
+
+
 def getCryptoMessage(msg: str, cmc_api: str) -> str:
     url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
-    if not cmc_api:
-        raise EnvironmentError(f"API key for CMC is empty")
 
     parameters = {"slug": CMC.crypto[msg]["name"], "convert": "USD"}
 
